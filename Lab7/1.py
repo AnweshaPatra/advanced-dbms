e57d518a298dbf2b9ba95c8b63b926c974af2168
Anwesha <36663339+AnweshaPatra@users.noreply.github.com>
2023-03-19 13:26:34 +0530

    Update 1.py

--- Lab7/1.py
+++ Lab7/1.py
@@ -6,7 +6,7 @@ cnx = mysql.connector.connect(user='root', password='', host='localhost', databa
 # Create a view of instructors without their salary called faculty
 cursor = cnx.cursor()
 cursor.execute("""
-    CREATE OR REPLACE VIEW faculty AS
+    CREATE VIEW faculty AS
     SELECT ID, name, dept_name
     FROM instructors;
 """)
@@ -14,57 +14,31 @@ cnx.commit()
 
 # Create a view of department salary totals
 cursor.execute("""
-    CREATE OR REPLACE VIEW department_salary_totals AS
+    CREATE VIEW dept_salary_totals AS
     SELECT dept_name, SUM(salary) AS total_salary
     FROM instructors
     GROUP BY dept_name;
 """)
 cnx.commit()
 
-# Create a role of student
-cursor.execute("""
-    CREATE ROLE student;
-""")
+# Create a user and assign the role of student
+cursor.execute("CREATE USER 'student' IDENTIFIED BY 'password'")
 cnx.commit()
 
-# Give select privileges on the view faculty to the role student
-cursor.execute("""
-    GRANT SELECT ON faculty TO student;
-""")
+# Grant SELECT privileges on the view faculty to the user student
+cursor.execute("GRANT SELECT ON faculty TO 'student'")
 cnx.commit()
 
-# Create a new user and assign her the role of student
-cursor.execute("""
-    CREATE USER 'newuser'@'localhost' IDENTIFIED BY 'password';
-""")
-cnx.commit()
-
-cursor.execute("""
-    GRANT student TO 'newuser'@'localhost';
-""")
-cnx.commit()
-
-# Revoke privileges of the new user
-cursor.execute("""
-    REVOKE SELECT ON faculty FROM 'newuser'@'localhost';
-""")
+# Revoke privileges of the user
+cursor.execute("REVOKE SELECT ON faculty FROM 'student'")
 cnx.commit()
 
 # Remove the role of student
-cursor.execute("""
-    REVOKE student FROM 'newuser'@'localhost';
-""")
+cursor.execute("DROP USER 'student'")
 cnx.commit()
 
-cursor.execute("""
-    DROP USER 'newuser'@'localhost';
-""")
-cnx.commit()
-
-# Give select privileges on the view faculty to the new user
-cursor.execute("""
-    GRANT SELECT ON faculty TO 'newuser'@'localhost';
-""")
+# Give SELECT privileges on the view faculty to the new user
+cursor.execute("GRANT SELECT ON faculty TO 'new_user'")
 cnx.commit()
 
 # Close the connection
